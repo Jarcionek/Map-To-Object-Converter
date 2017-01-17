3b6ea9d6b21f855cc4f79617809a39549a2430c3
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2017-01-17 11:16:50 +0000

    moved release related TODOs from TODOs.md to release.sh

--- TODOs.md
+++ TODOs.md
@@ -1,25 +1,3 @@
-1. Automate release
-  - change version (and commit)
-  - git tag
-  - generate files
-    - `mvn package`
-    - `mvn source:jar`
-    - `mvn javadoc:jar`
-    - pom file
-  - sign files:
-    - `gpg -ab filename`
-    - `gpg --verify filename`
-  - upload files:
-    - `mvn deploy:deploy-file -DgroupId=uk.co.jpawlak -DartifactId=map-to-object-converter -Dversion=1.0 -DgeneratePom=false -Dpackaging=jar -DrepositoryId=sonatype -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DpomFile=pom.xml -Dfile=target/map-to-object-converter-1.0.jar`
-    - repositoryId is the id of the server with user credentials saved in `.m2/settings.xml`
-    - to do this manually, go to `oss.sonatype.org / Staging Upload` and select the files
-    - in `Staging Repositories` select repository and press `Release` at the top
-  - change version (and commit)
-  - update changelog
-  - update version in quick start in readme
-  - push changes to remote (together with tags):
-    - `git push --tags`
+1. bounded wildcards for Optional fields
 
-2. bounded wildcards for Optional fields
-
-3. Multi value to single field register-able converters
+2. Multi value to single field register-able converters
--- release.sh
+++ release.sh
@@ -1,31 +1,57 @@
 #!/usr/bin/env bash
 
+#TODO: note that it has to be used in git bash, btw why it doesn't work in cygwin...? Maybe also some automated check...
+
+#TODO: add error handling - currently if anything fails at any point, the script still continues producing more errors
+
+# GET USER INPUT
 echo 'ENTER RELEASE VERSION:'
 read version
 echo 'ENTER GPG PASSPHRASE:'
 read passphrase
+#TODO: passphrase is visible on the console and then in the history, 'reset ; history -c' followed by what version is being released?
 
+# UPDATE VERSION IN POM FILE
 sed "s;^    <version>.*</version>;    <version>${version}</version>;" pom.xml > pom.xml2
 rm pom.xml
 mv pom.xml2 pom.xml
-
 git add pom.xml
+
+#TODO: update changelog with version and date (something smart checking the top line?)
+#TODO: update quick-start in readme with new version (should it be here before actual release?)
+
+# COMMIT, TAG AND PUSH
 git commit -m "prepare release map-to-object-converter-${version}"
 git tag "${version}"
 
+# BUILD THE ARTIFACTS
 rm -rf target/
-
 mvn package
 mvn source:jar
 mvn javadoc:jar
 
+# COPY THE ARTIFACTS TO target/release
 mkdir target/release
-
 cp pom.xml target/release/map-to-object-converter-${version}.pom
 cp target/*.jar target/release
 
+# SIGN THE ARTIFACTS
 cd target/release
 echo "${passphrase}" | gpg --passphrase-fd 0 -ab map-to-object-converter-${version}.pom
 echo "${passphrase}" | gpg --passphrase-fd 0 -ab map-to-object-converter-${version}.jar
 echo "${passphrase}" | gpg --passphrase-fd 0 -ab map-to-object-converter-${version}-javadoc.jar
 echo "${passphrase}" | gpg --passphrase-fd 0 -ab map-to-object-converter-${version}-sources.jar
+
+#TODO: update pom file with new snapshot version
+#TODO: add new header for changelog
+#TODO: git commit and push
+
+# FINAL NOTE
+echo "MANUAL STEP REQUIRED"
+echo "Go to 'oss.sonatype.org / Staging Upload' and upload the files from 'target/release'"
+echo "Then go to 'Staging Repositories', select repository and press 'Release' at the top"
+
+#TODO: optional push (or maybe that should be manual so the verification is possible?)
+# OPTIONAL PUSH
+#git push
+#git push --tags
